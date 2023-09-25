3737803ab9e14956d3a590a35182d692a7ac9a6b
Sebastian Rios <sebastianriosrico@gmail.com>
2023-09-25 11:32:16 -0500

    Cascaron del proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
+import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
+import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso/ingreso-egreso.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    DetalleComponent,
+    EstadisticaComponent,
+    IngresoEgresoComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
